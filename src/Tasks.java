a8aa01cf1384aa44987c2cfecefd3c4c4c08c079
Zsolt Nagy <nagyza@gmail.com>
2017-04-06 13:58:51 +0200

    feat(app) add methods
    changeDone() method
    toString() method
    toFile() method

--- src/Tasks.java
+++ src/Tasks.java
@@ -3,12 +3,26 @@ public class Tasks {
   private String isDone;
   private String text;
 
-  public Tasks(String isDone, String text) {
-    this.isDone = isDone;
+  public Tasks(String text) {
+    this.isDone = " ";
     this.text = text;
   }
 
-  public String toFile() {
+  public void changeDone() {
+    if (this.isDone == " ") {
+      this.isDone = "x";
+    } else {
+      this.isDone = " ";
+    }
+  }
+
+  @Override
+  public String toString() {
     return "[" + isDone + "] " + text;
   }
+
+  String toFile() {
+    return isDone + ";" + text;
+  }
+
 }
