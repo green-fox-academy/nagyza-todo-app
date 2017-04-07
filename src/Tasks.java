7552b7197a9a2c0539b914cc5e2c7aca45a53be0
Zsolt Nagy <nagyza@gmail.com>
2017-04-07 10:47:13 +0200

    feat(app) new variable priority to prepare tasks to sorting
    New variable date to prepare add a timestamp to added tasks

--- src/Tasks.java
+++ src/Tasks.java
@@ -2,15 +2,25 @@
 public class Tasks {
   private String isDone;
   private String text;
+  private String date;
+  private int priority;
 
   Tasks(String text) {
     this.isDone = " ";
     this.text = text;
+    this.date = "";
+    this.priority = 2;
   }
 
-  Tasks(String isDone, String text) {
+  Tasks(String isDone, String text, String date, int priority) {
     this.isDone = isDone;
     this.text = text;
+    this.date = date;
+    this.priority = priority;
+  }
+
+  public int getPriority() {
+    return priority;
   }
 
   void changeDone() {
@@ -31,11 +41,11 @@ public class Tasks {
 
   @Override
   public String toString() {
-    return "[" + isDone + "] " + text;
+    return "[" + isDone + "] " + " Priority: " + priority + "  " + date + "\t" + text;
   }
 
   String toFile() {
-    return isDone + ";" + text;
+    return isDone + ";" + text + ";" + date + ";" + priority;
   }
 
 }
